428f7ffce0d54a9ce64ec79bfab1589ffa677614
Tom Chapman <tchapman000@gmail.com>
2025-11-16 22:33:25 -0800

    deploy test

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}`;
+console.log("MongoDB URI:", uri);
 
 let client;
 let db;
